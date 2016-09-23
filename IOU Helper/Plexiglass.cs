953d8d7bc8c3903d24c69309ae5ea429170c0995
Jeremy Shaw <puremana@gmail.com>
2016-09-23 20:10:02 +1200

    Finished the statistics panel with resizing based off the client size. Added all of the proper labels.

--- IOU Helper/Plexiglass.cs
+++ IOU Helper/Plexiglass.cs
@@ -66,6 +66,7 @@ public partial class Plexiglass : Form
     {
         InitializeComponent();
 
+        //this.DoubleBuffered = true;
         this.TransparencyKey = Color.Turquoise;
         this.BackColor = Color.Turquoise;
         this.Opacity = 0.9;      // Tweak as desired
@@ -96,6 +97,16 @@ public partial class Plexiglass : Form
     {
         // Ensure the plexiglass keeps the owner covered
         this.ClientSize = this.Owner.ClientSize;
+
+        int clientWidth = this.Width;
+        int clientHeight = this.Height;
+        int p1LocationWidth = Convert.ToInt32(clientWidth * 0.64);
+        int p1LocationHeight = Convert.ToInt32(clientHeight * 0.60); //0.32
+        int p1SizeWidth = Convert.ToInt32(clientWidth * 0.24);
+        int p1SizeHeight = Convert.ToInt32(clientHeight * 0.19);
+
+        panel1.Location = new System.Drawing.Point(p1LocationWidth, p1LocationHeight);
+        panel1.Size = new System.Drawing.Size(p1SizeWidth, p1SizeHeight);
     }
     protected override void OnFormClosing(FormClosingEventArgs e)
     {
@@ -114,57 +125,47 @@ public partial class Plexiglass : Form
         // Always keep the owner activated instead
         this.BeginInvoke(new Action(() => this.Owner.Activate()));
     }
-
-    private GroupBox groupBox1;
-    private Label label2;
-    private Label label1;
     private GroupBox groupBox2;
     private TextBox textBox2;
     private TextBox textBox1;
+    private Panel panel1;
+    private Label label13;
+    private Label label14;
+    private Label label15;
+    private Label label16;
+    private Label label17;
+    private Label label18;
+    private Label label19;
+    private Label label20;
+    private Label label21;
+    private Label label22;
+    private Label label23;
+    private Label label24;
     private const int DWMWA_TRANSITIONS_FORCEDISABLED = 3;
     [DllImport("dwmapi.dll")]
     private static extern int DwmSetWindowAttribute(IntPtr hWnd, int attr, ref int value, int attrLen);
 
     private void InitializeComponent()
     {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(80, 70);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(347, 333);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Statistics";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 55);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 32);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "blah";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 55);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Blah : ";
             // 
             // groupBox2
             // 
@@ -176,6 +177,14 @@ public partial class Plexiglass : Form
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Console";
+            this.groupBox2.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 289);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(391, 38);
+            this.textBox2.TabIndex = 1;
             // 
             // textBox1
             // 
@@ -185,24 +194,186 @@ public partial class Plexiglass : Form
             this.textBox1.Size = new System.Drawing.Size(391, 216);
             this.textBox1.TabIndex = 0;
             // 
-            // textBox2
+            // panel1
             // 
-            this.textBox2.Location = new System.Drawing.Point(7, 289);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(391, 38);
-            this.textBox2.TabIndex = 1;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.label15);
+            this.panel1.Controls.Add(this.label16);
+            this.panel1.Controls.Add(this.label17);
+            this.panel1.Controls.Add(this.label18);
+            this.panel1.Controls.Add(this.label19);
+            this.panel1.Controls.Add(this.label20);
+            this.panel1.Controls.Add(this.label21);
+            this.panel1.Controls.Add(this.label22);
+            this.panel1.Controls.Add(this.label23);
+            this.panel1.Controls.Add(this.label24);
+            this.panel1.Location = new System.Drawing.Point(88, 462);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(451, 325);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(100, 92);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(131, 38);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "unknown";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(2, 92);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(165, 38);
+            this.label14.TabIndex = 22;
+            this.label14.Text = "Best cards :";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(100, 74);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(131, 38);
+            this.label15.TabIndex = 21;
+            this.label15.Text = "unknown";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(2, 74);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(93, 38);
+            this.label16.TabIndex = 20;
+            this.label16.Text = "Nets :";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(100, 56);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(131, 38);
+            this.label17.TabIndex = 19;
+            this.label17.Text = "unknown";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(2, 56);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(195, 38);
+            this.label18.TabIndex = 18;
+            this.label18.Text = "Mining NRG :";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(100, 38);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(131, 38);
+            this.label19.TabIndex = 17;
+            this.label19.Text = "unknown";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(2, 36);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(179, 38);
+            this.label20.TabIndex = 16;
+            this.label20.Text = "Wood NRG :";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(100, 18);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(131, 38);
+            this.label21.TabIndex = 15;
+            this.label21.Text = "unknown";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(2, 18);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(116, 38);
+            this.label22.TabIndex = 14;
+            this.label22.Text = "G/min :";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.Location = new System.Drawing.Point(100, 2);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(131, 38);
+            this.label23.TabIndex = 13;
+            this.label23.Text = "unknown";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Font = new System.Drawing.Font("Segoe UI", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label24.Location = new System.Drawing.Point(2, 2);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(131, 38);
+            this.label24.TabIndex = 12;
+            this.label24.Text = "XP/min :";
             // 
             // Plexiglass
             // 
             this.ClientSize = new System.Drawing.Size(1058, 877);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
             this.Name = "Plexiglass";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.ResizeEnd += new System.EventHandler(this.Plexiglass_ResizeEnd);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+
+    }
+
+    private void Plexiglass_Resize(object sender, EventArgs e)
+    {
+
+    }
+
+    private void panel1_Paint(object sender, PaintEventArgs e)
+    {
+        if (panel1.BorderStyle == BorderStyle.FixedSingle)
+        {
+            int thickness = 2;
+            int halfThickness = thickness / 2;
+            using (Pen p = new Pen(Color.Black, thickness))
+            {
+                e.Graphics.DrawRectangle(p, new Rectangle(halfThickness,
+                                                          halfThickness,
+                                                          panel1.ClientSize.Width - thickness,
+                                                          panel1.ClientSize.Height - thickness));
+            }
+        }
+    }
+
+    private void Plexiglass_ResizeEnd(object sender, EventArgs e)
+    {
 
     }
 }
